a21391aafdeafc387f67d13f318a7797c63ff3f6
MyNextWeekend <jinhu007@outlook.com>
2024-03-19 21:22:29 +0800

    结构体所有权

--- src/a_struct_test.rs
+++ src/a_struct_test.rs
@@ -21,9 +21,9 @@ impl Student {
         self.name = name
     }
 
-    fn avg_age(students: &[Student]) -> f32 {
+    fn avg_age(students: Vec<&Student>) -> f32 {
         let mut sum = 0.0;
-        for student in students.iter() {
+        for &student in students.iter() {
             sum += student.age as f32
         }
         sum / (students.len() as f32)
@@ -38,6 +38,8 @@ fn test_01() {
     s1.set_name(String::from("新章三"));
 
     println!("s1_name:{},s2_name:{},s3_name:{}",s1.get_name(),s2.get_name(),s3.get_name());
-    let stu = [s1,s2,s3];
-    println!("平均年龄是：{}",Student::avg_age(&stu));
+    let stu = vec![&s1,&s2,&s3];
+    println!("平均年龄是：{}",Student::avg_age(stu.clone()));
+    println!("s1_name:{},s2_name:{},s3_name:{}",s1.get_name(),s2.get_name(),s3.get_name());
+    println!("平均年龄是：{}",Student::avg_age(stu));
 }
