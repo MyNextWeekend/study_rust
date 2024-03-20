a2099ec616e641eb3d46699fc8c7c5bc6328399d
MyNextWeekend <jinhu007@outlook.com>
2024-03-20 20:47:09 +0800

    整理文件夹

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
 // 也可以不保留lib.rs 直接写在mian.rs 中
 // ====== 方式一（只导出一级模块名称） =====
-pub mod a_enum_test;
-pub mod a_option_test;
-pub mod a_vec_test;
-pub mod a_error_test;
-pub mod a_map_test;
-
-pub mod a_struct_test;
-
+pub mod study;
+pub mod study_other;
 // ====== 方式二（导出模块下所有子内容） =====
 // mod vec_test;
 
---
+++ src/study/mod.rs
@@ -0,0 +1,5 @@
+pub mod a_option_test;
+pub mod a_vec_test;
+pub mod a_error_test;
+pub mod a_map_test;
+pub mod a_struct_test;
