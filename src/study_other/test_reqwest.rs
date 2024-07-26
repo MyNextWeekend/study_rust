52dd63721ac09784251403f6d606b511a9d23f52
MyNextWeekend <jinhu007@outlook.com>
2024-07-26 23:16:17 +0800

    提交

--- src/study_other/test_reqwest.rs
+++ src/study_other/test_reqwest.rs
@@ -13,28 +13,24 @@ impl HttpClient {
         Ok(resp)
     }
     // 异步get请求
-    pub async fn get(url: &str) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+    pub async fn get(url: &str) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
         let client = reqwest::Client::new(); //可以自定义客户端，重复利用
         let resp = client
             .get(url)
             .send()
-            .await?
-            .json::<HashMap<String, String>>()
             .await?;
         println!("拿到结果是: {:#?}", resp);
         Ok(resp)
     }
 
     // 异步post请求
-    pub async fn post<T: std::fmt::Debug+ DeserializeOwned>(url: &str, body: &HashMap<String, String>) -> Result<T, Box<dyn std::error::Error>> {
+    pub async fn post(url: &str, body: &HashMap<String, String>) -> Result<reqwest::Response, Box<dyn std::error::Error>> {
         let client = reqwest::Client::new(); //可以自定义客户端，重复利用
         let resp = client
             .post(url)
             .header("Content-Type".to_string(), "application/json".to_string())
             .json(&body)
             .send()
-            .await?
-            .json::<T>()
             .await?;
         println!("拿到结果是: {:?}", resp);
         Ok(resp)
@@ -75,7 +71,7 @@ mod reqwest_test {
         data.insert("file_id".into(), "98997".into());
         data.insert("file_type".into(), "jog".into());
         data.insert("file_size".into(), "45646".into());
-        let res = HttpClient::post::<HashMap<String, String>>(&url, &data).await;
+        let res = HttpClient::post(&url, &data).await;
         match res {
             Ok(res) => println!("收到数据: {:?}", res),
             Err(err) => println!("异常了呀: {}", err.to_string()),
