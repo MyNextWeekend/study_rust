9a8e04f0800f478f5b4a22f3020b2c371686a9d7
MyNextWeekend <jinhu007@outlook.com>
2024-07-29 20:19:37 +0800

    calamine操作

--- src/study_other/test_calamine.rs
+++ src/study_other/test_calamine.rs
@@ -1,16 +1,27 @@
+use calamine::{open_workbook, Error, Xlsx, Reader, RangeDeserializerBuilder};
+
+
+pub fn read_xlsx(file_path: &str) -> Result<(), Error> {
+    let mut work_book: Xlsx<_> = open_workbook(file_path)?;
+    let sheet = work_book.worksheet_range("Sheet1")
+        .map_err(|_| calamine::Error::Msg("Cannot find Sheet1"))?;
+    let iter_records =
+        RangeDeserializerBuilder::with_headers(&["name", "age"]).from_range(&sheet)?;
+
+    for result in iter_records {
+        let (label, value): (String, f64) = result?;
+        println!("name={:?}, age={:?}", label, value);
+    }
+    Ok(())
+}
+
+
 #[cfg(test)]
 mod calamine_test {
-    use calamine::{open_workbook, Reader, Xlsx};
+    use crate::study_other::test_calamine::read_xlsx;
 
     #[test]
     fn test01() {
-        let workbook: Result<Xlsx<_>, _> = open_workbook("file.xlsx");
-        if let Ok(mut excel) = workbook {
-            if let Ok(data) = excel.worksheet_range("Sheet1") {
-                for row in data.rows() {
-                    println!("row={:?}, row[0]={:?}", row, row[0]);
-                }
-            }
-        }
+        read_xlsx("/Users/weekend/Desktop/test.xlsx");
     }
 }
