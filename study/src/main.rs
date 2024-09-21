15a6ee8d7dc78bcd74f23d1f09081c519051fe19
MyNextWeekend <jinhu007@outlook.com>
2024-09-21 20:35:34 +0800

    修改为多个creat

---
+++ study/src/main.rs
@@ -0,0 +1,18 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+mod test_enum;
+mod test_error;
+mod test_generic;
+mod test_io;
+mod test_map;
+mod test_option;
+mod test_struct;
+mod test_thread;
+mod test_time;
+mod test_trait;
+mod test_vec;
+
+fn main() {
+    println!("Hello from study!");
+}
---
+++ study_other/src/main.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+mod test_anyhow;
+mod test_calamine;
+mod test_chrono;
+mod test_clap;
+mod test_env_logger;
+mod test_reqwest;
+mod test_serde;
+mod test_sqlx;
+mod test_thiserror;
+
+fn main() {
+    println!("Hello from study_other!");
+}
