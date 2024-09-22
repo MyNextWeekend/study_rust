d99382445a4373bc3081145c4208ab09d0be17d6
MyNextWeekend <jinhu007@outlook.com>
2024-09-22 14:46:12 +0800

    整理笔记

--- study/src/test_enum.rs
+++ study/src/test_enum.rs
@@ -1,19 +1,33 @@
+/// 方式一：结构体中直接写字段名称
+#[derive(Debug)]
+enum IpAddr {
+    V4,
+    V6,
+}
+
+/// 方式二：将数据附加到枚举的变体中（利用struct）
+#[derive(Debug)]
+struct IpAddrWithStr {
+    kind: IpAddr,
+    address: String,
+}
+
+/// 方式三：将数据附加到枚举的变体中, 优点：不需要额外使用struct，每个变体拥有不同的类型以及关联的数量
+#[derive(Debug)]
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+/// 可以枚举添加方法
+impl IpAddrKind {
+    fn call(&self) {
+        println!("值是:{:?}", self)
+    }
+}
 #[cfg(test)]
 mod enum_test {
-    // 方式一：结构体中直接写字段名称
-    #[derive(Debug)]
-    enum IpAddr {
-        V4,
-        V6,
-    }
-
-    // 方式二：将数据附加到枚举的变体中（利用struct）
-    #[derive(Debug)]
-    struct IpAddrWithStr {
-        kind: IpAddr,
-        address: String,
-    }
-
+    use super::{IpAddrWithStr, IpAddr, IpAddrKind};
 
     #[test]
     fn test_01() {
@@ -29,29 +43,16 @@ mod enum_test {
         println!("ip类型: {:?} 地址是:{}", loopback.kind, loopback.address);
     }
 
-    // 方式三：将数据附加到枚举的变体中, 优点：不需要额外使用struct，每个变体拥有不同的类型以及关联的数量
-    #[derive(Debug)]
-    enum IpAddrKind {
-        V4(u8, u8, u8, u8),
-        V6(String),
-    }
-
-    //可以为枚举添加方法
-    impl IpAddrKind {
-        fn call(&self) {
-            println!("值是:{:?}", self)
-        }
-    }
 
     #[test]
     fn test_02() {
         let home = IpAddrKind::V4(127, 0, 0, 1);
         let loopback = IpAddrKind::V6(String::from("::1"));
-        home.call();//调用枚举的方法
+        home.call(); //调用枚举的方法
         loopback.call();
         match home {
-            IpAddrKind::V4(a, b, c, d) => {println!("{}-{}-{}-{}",a,b,c,d)}
-            IpAddrKind::V6(v) => {println!("{}",v)}
+            IpAddrKind::V4(a, b, c, d) => { println!("{}-{}-{}-{}", a, b, c, d) }
+            IpAddrKind::V6(v) => { println!("{}", v) }
         }
     }
 }
--- study/src/test_io.rs
+++ study/src/test_io.rs
@@ -1,37 +1,71 @@
+use std::fs::{File, OpenOptions, remove_file};
+use std::io;
+use std::io::{BufRead, BufReader, Read, Write};
+
+
+/// 创建并写入到文件
+fn create_write_file(file_path: &str, content: &str) -> Result<usize, io::Error> {
+    let mut file = File::create(&file_path)?;
+    let num = file.write(content.as_bytes())?;
+    println!("文件路径:{file_path} 成功写入{num}字节", );
+    Ok(num)
+}
+
+/// 追加模式往文件中写入
+fn append_write_file(file_path: &str, content: &str) -> Result<usize, io::Error> {
+    let mut file = OpenOptions::new().append(true).open(&file_path)?;
+    let num = file.write(content.as_bytes())?;
+    println!("文件路径:{file_path} 成功追加写入{num}字节", );
+    Ok(num)
+}
+
+fn read_file(file_path: &str) -> Result<String, io::Error> {
+    let mut file = File::open(&file_path)?;
+    let mut result = String::new();
+    file.read_to_string(&mut result)?;
+    println!("整个读取文件内容是: {}", result);
+    Ok(result)
+}
+
+/// 按行遍历文件内容
+fn read_file_iter(file_path: &str) -> Result<io::Lines<BufReader<File>>, io::Error> {
+    let file = File::open(&file_path)?;
+    Ok(BufReader::new(file).lines())
+}
+
+/// 删除文件
+fn del_file(file_path: &str) -> Result<(), io::Error> {
+    let _ = remove_file(file_path)?;
+    println!("删除文件成功");
+    Ok(())
+}
+
 #[cfg(test)]
 mod io_test {
-    use std::fs::{File, OpenOptions};
-    use std::io::{BufRead, BufReader, Read, Write, BufWriter};
+    use super::*;
 
     #[test]
     fn test_01() {
         let file_path = "./test.txt";
         println!("========= 创建并写入 ===========");
-        if let Ok(mut file) = File::create(&file_path) {
-            &file.write("写入内容-->abcef123456...\n".as_bytes()).unwrap();
-            println!("文件:{:?}", file);
-        } else {
-            println!("文件创建失败");
-        }
+        create_write_file(&file_path, "这个是需要写入的内容\n").unwrap();
 
 
         println!("========= 追加模式写入 ===========");
-        let mut file = OpenOptions::new().append(true).open(&file_path).unwrap();
-        &file.write("追加内容-->abcef123456...\n".as_bytes()).unwrap();
-        println!("文件:{:?}", file);
+        append_write_file(&file_path, "这个是追加的内容\n").unwrap();
+
 
         println!("========= 整个读取 ===========");
-        let mut file = File::open(&file_path).unwrap();
-        let mut result = String::new();
-        file.read_to_string(&mut result).unwrap();
-        println!("整个读取文件内容是: {}", result);
+        read_file(&file_path).unwrap();
 
         println!("========= 按行读取 ===========");
-        let file = File::open(&file_path).unwrap();
-        for line in BufReader::new(file).lines() {
+        for line in read_file_iter(&file_path).unwrap() {
             if let Ok(txt) = line {
-                println!("按照行读取: {}", txt);
+                println!("{txt}");
             }
         }
+
+        println!("========= 删除文件 ===========");
+        del_file(&file_path).unwrap();
     }
 }
